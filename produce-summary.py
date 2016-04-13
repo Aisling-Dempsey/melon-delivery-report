20885a6484fb4fc26849ea13ecea3370a3a01e8e
Nymarra <aisling.n.dempsey@gmail.com>
2016-04-12 20:38:39 -0700

    Initial commit (complete)

---
+++ produce-summary.py
@@ -0,0 +1,28 @@
+def count(file):
+    for line in file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print "Delivered {} {}s for total of ${}".format(
+            count, melon, amount)
+
+print "Day 1"
+the_file = open("um-deliveries-20140519.txt")
+count(the_file)
+the_file.close()
+
+
+print "Day 2"
+the_file = open("um-deliveries-20140520.txt")
+count(the_file)
+the_file.close()
+
+
+print "Day 3"
+the_file = open("um-deliveries-20140521.txt")
+count(the_file)
+the_file.close()
